45f4780792655c3a12f76d7606066aadb69c9082
marvin <mcanastuj1999@gmail.com>
2026-04-25 19:35:04 -0600

    hashrouter added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/useAuth.jsx'
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    
+    <HashRouter>
         <AuthProvider>
             <App />
         </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
 )
